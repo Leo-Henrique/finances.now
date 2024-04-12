59add6818d02ff2d49a374d6c24ab2e0be583046
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-04-12 19:18:44 -0300

    test(value-object): check match of passwords

--- src/domain/entities/value-objects/password-hash.spec.ts
+++ src/domain/entities/value-objects/password-hash.spec.ts
@@ -1,15 +1,25 @@
+import { faker } from "@faker-js/faker";
 import { compare } from "bcryptjs";
 import { describe, expect, it } from "vitest";
 import { PasswordHash } from "./password-hash";
 
+const password = "123456";
+
 describe("[Value Object] Password Hash", () => {
   it("should be able to create password hash", async () => {
-    const password = "123456";
-
     const { hash } = new PasswordHash(password);
     const isMatchedPassword = await compare(password, hash);
 
     expect(hash).not.toBe(password);
     expect(isMatchedPassword).toBeTruthy();
+  });
+
+  it("should be able to check if a string match a hash", async () => {
+    const passwordObject = new PasswordHash(password);
+    const validPassword = passwordObject.match(password);
+    const invalidPassword = passwordObject.match(faker.internet.password());
+
+    expect(validPassword).toBeTruthy();
+    expect(invalidPassword).toBeFalsy();
   });
 });
