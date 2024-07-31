3aa94ba032827d2bd838419085120197d9babaf9
leo-henrique <leonardo0507.business@gmail.com>
2024-07-31 15:29:44 -0300

    chore(infra): start infra with @leo-h/create-nodejs-app

--- src/infra/env.ts
+++ src/infra/env.ts
@@ -1,16 +1,14 @@
-import "dotenv/config";
+import packageJson from "@/../package.json";
+import { config } from "dotenv";
 import { z } from "zod";
+
+config({ override: true });
 
 const schema = z.object({
   NODE_ENV: z.enum(["test", "development", "production"]),
-  APP_NAME: z.string(),
-  APP_PORT: z.coerce.number(),
-  APP_ACCESS_CLIENT_SIDE_ALLOWED: z.string(),
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASS: z.string(),
-  POSTGRES_HOST: z.string(),
-  POSTGRES_PORT: z.coerce.number(),
-  POSTGRES_NAME: z.string(),
+  API_NAME: z.string().default(packageJson.name),
+  API_PORT: z.coerce.number().default(3333),
+  API_ACCESS_PERMISSION_CLIENT_SIDE: z.string().default("*"),
 });
 
 const parsedEnv = schema.safeParse(process.env);
--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "./app";
-import { env } from "./env";
-
-(async () => {
-  const serverName = await app.listen({ port: env.APP_PORT });
-
-  console.log(`Application "${env.APP_NAME}" is running!`);
-  console.log(serverName);
-})();
